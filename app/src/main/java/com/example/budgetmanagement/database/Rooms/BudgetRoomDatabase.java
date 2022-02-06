504146321e27f3d21f55eac37bc5dd6e10090ac2
Broslaws <pawelgawrych203@gmail.com>
2022-02-06 21:36:28 +0100

    Create BudgetRoomDatabase

---
+++ app/src/main/java/com/example/budgetmanagement/database/Rooms/BudgetRoomDatabase.java
@@ -0,0 +1,59 @@
+package com.example.budgetmanagement.database.Rooms;
+
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.budgetmanagement.database.Rooms.Category.Category;
+import com.example.budgetmanagement.database.Rooms.Category.CategoryDao;
+import com.example.budgetmanagement.database.Rooms.Transaction.Transaction;
+import com.example.budgetmanagement.database.Rooms.Transaction.TransactionDao;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Transaction.class, Category.class}, version = 2, exportSchema = false)
+public abstract class BudgetRoomDatabase extends RoomDatabase {
+
+    public abstract TransactionDao transactionDao();
+    public abstract CategoryDao categoryDao();
+
+    private static volatile BudgetRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static BudgetRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (BudgetRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            BudgetRoomDatabase.class, "BudgetManagement")
+                            .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+//  Populate database at first run
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            databaseWriteExecutor.execute(() -> {
+//                CategoryDao categoryDao = INSTANCE.categoryDao();
+//                Category category = new Category(0, "Inne", 0, LocalDate.now(), 0);
+//                categoryDao.insert(category);
+            });
+        }
+    };
+}
