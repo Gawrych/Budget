58d91393f49ee066737dbec341036adb9fc5aabf
Broslaws <pawelgawrych203@gmail.com>
2022-04-03 20:06:09 +0200

    Working version of sorting

--- app/src/main/java/com/example/budgetmanagement/ui/History/HistoryBottomSheetOrder.java
+++ app/src/main/java/com/example/budgetmanagement/ui/History/HistoryBottomSheetOrder.java
@@ -31,7 +31,6 @@ public class HistoryBottomSheetOrder extends Fragment {
     private CheckBox profitCheckBox;
     private CheckBox reversedCheckBox;
     private CheckBox filterByProfitCheckBox;
-    private int[] lastFilters;
 
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -46,12 +45,6 @@ public class HistoryBottomSheetOrder extends Fragment {
         profitCheckBox = bottomSheetDialog.findViewById(R.id.profitCheck);
         reversedCheckBox = bottomSheetDialog.findViewById(R.id.reversedCheck);
         filterByProfitCheckBox = bottomSheetDialog.findViewById(R.id.filterByProfit);
-
-        lastFilters = new int[4];
-        lastFilters[0] = 0;
-        lastFilters[1] = 0;
-        lastFilters[2] = 0;
-        lastFilters[3] = 0;
 
 //        Change this, this show and hide button to filter by profit
         assert filterByProfitCheckBox != null;
@@ -74,23 +67,9 @@ public class HistoryBottomSheetOrder extends Fragment {
         dateSort.setOnClickListener(v -> sortByDate());
     }
 
-    public BottomSheetDialog getBottomSheetDialog() {
-        return bottomSheetDialog;
-    }
-
     public void sort(LiveData<List<HistoryAndTransaction>> historyAndTransactionList) {
         this.sortedHistoryAndTransactionList = historyAndTransactionList;
-
-        Log.d("CheckError", String.valueOf(lastFilters[0]));
-        profitCheckBox.setChecked(lastFilters[0] == 1);
-        filterByProfitCheckBox.setChecked(lastFilters[1] == 1);
-        reversedCheckBox.setChecked(lastFilters[2] == 1);
-
         bottomSheetDialog.show();
-    }
-
-    public LiveData<List<HistoryAndTransaction>> getSortedList() {
-        return sortedHistoryAndTransactionList;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -105,15 +84,11 @@ public class HistoryBottomSheetOrder extends Fragment {
             sortedHistoryAndTransactionList = Transformations.map(sortedHistoryAndTransactionList,
                     input -> input.stream().filter(o1 -> !o1.transaction.getProfit()).collect(Collectors.toList()));
         }
-
-        lastFilters[0] = profit ? 1 : 0;
-        lastFilters[1] = filterProfit ? 1 : 0;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void checkReversed() {
         boolean reversed = reversedCheckBox.isChecked();
-        lastFilters[2] = reversed ? 1 : 0;
         if (reversed) {
             sortedHistoryAndTransactionList = Transformations.map(sortedHistoryAndTransactionList,
                     input -> input.stream().collect(Collectors.collectingAndThen(Collectors.toList(), l -> {Collections.reverse(l); return l;})));
@@ -126,7 +101,6 @@ public class HistoryBottomSheetOrder extends Fragment {
         sortedHistoryAndTransactionList = Transformations.map(sortedHistoryAndTransactionList,
                 input -> input.stream().sorted(Comparator.comparing(o -> o.transaction.getTitle())).collect(Collectors.toList())); // Fix this
         checkReversed();
-        lastFilters[3] = 1;
         bottomSheetDialog.cancel();
     }
 
@@ -137,7 +111,6 @@ public class HistoryBottomSheetOrder extends Fragment {
                 input -> input.stream().sorted(Comparator.comparingDouble(o ->
                         o.transaction.getAmount())).collect(Collectors.toList()));
         checkReversed();
-        lastFilters[3] = 2;
         bottomSheetDialog.cancel();
     }
 
@@ -148,7 +121,13 @@ public class HistoryBottomSheetOrder extends Fragment {
                 input -> input.stream().sorted(Comparator.comparingLong(o ->
                         o.history.getAddDate())).collect(Collectors.toList()));
         checkReversed();
-        lastFilters[3] = 3;
         bottomSheetDialog.cancel();
+    }
+
+    public BottomSheetDialog getBottomSheetDialog() {
+        return bottomSheetDialog;
+    }
+    public LiveData<List<HistoryAndTransaction>> getSortedList() {
+        return sortedHistoryAndTransactionList;
     }
 }
