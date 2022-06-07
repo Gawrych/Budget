c9405ad05cd9f57872d43fdeab57f0941ff09c78
Broslaws <pawelgawrych203@gmail.com>
2022-06-07 18:08:42 +0200

    Divide functions

--- app/src/main/java/com/example/budgetmanagement/ui/History/TransactionDataCollectorFromUser.java
+++ app/src/main/java/com/example/budgetmanagement/ui/History/TransactionDataCollectorFromUser.java
@@ -16,44 +16,125 @@ import java.time.format.DateTimeFormatter;
 
 public class TransactionDataCollectorFromUser {
 
+    private final View root;
     private float amount;
     private String title;
     private long date;
-    private boolean profit;
+    private boolean isProfit;
     private int categoryId;
+    private EditText titleField;
+    private EditText amountField;
+    private boolean isEmpty;
 
-    public TransactionDataCollectorFromUser() {}
+    public TransactionDataCollectorFromUser(View root) {
+        this.root = root;
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public boolean collectData(View root, EditText calendar, int categoryId) {
-        EditText titleField = root.findViewById(R.id.title);
-        EditText amountField = root.findViewById(R.id.amount);
-        this.categoryId = categoryId;
+    public boolean collectData(EditText calendar, int categoryId) {
+        initializeFields(categoryId);
 
-        title = titleField.getText().toString();
-        if (title.length() == 0) {
-            titleField.setError(root.getContext().getString(R.string.field_cant_be_empty));
+        setTitle();
+        if (isEmpty) {
             return false;
         }
 
-        String amountInString = amountField.getText().toString();
-        if (amountInString.length() == 0) {
-            amountField.setError(root.getContext().getString(R.string.field_cant_be_empty));
-            return false;
-        } else {
-            amount = Float.parseFloat(amountInString);
+        setAmount();
+        if (isEmpty) {
+             return false;
         }
 
-        RadioGroup radioGroup = root.findViewById(R.id.radioGroup);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        LocalDate parsedDate = LocalDate.parse(calendar.getText(), formatter);
-        date = parsedDate.toEpochDay();
-
-        int profitIcon = radioGroup.getCheckedRadioButtonId();
-        profit = profitIcon == R.id.profitTrue;
+        setDateInPattern(calendar);
+        setProfit();
 
         return true;
+    }
+
+    private void initializeFields(int categoryId) {
+        setCategoryId(categoryId);
+        getTitleField();
+        getAmountField();
+    }
+
+    private void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    private void getTitleField() {
+         titleField = root.findViewById(R.id.title);
+    }
+
+    private void getAmountField() {
+        amountField = root.findViewById(R.id.amount);
+    }
+
+    private void setTitle() {
+        getTextFromTitle();
+        checkLength(title);
+        if (isEmpty) {
+            setEmptyFieldErrorMessage(titleField);
+        }
+    }
+
+    private void getTextFromTitle() {
+        title = titleField.getText().toString();
+    }
+
+    private void checkLength(String value) {
+        isEmpty = value.length() == 0;
+    }
+
+    private void setEmptyFieldErrorMessage(EditText field) {
+        field.setError(root.getContext().getString(R.string.field_cant_be_empty));
+    }
+
+    public void setAmount() {
+        String amountInString = getTextFromAmount();
+        checkLength(amountInString);
+        if (isEmpty) {
+            setEmptyFieldErrorMessage(amountField);
+        } else {
+            initializeAmountVariable(amountInString);
+        }
+    }
+
+    public String getTextFromAmount() {
+        return amountField.getText().toString();
+    }
+
+    public void initializeAmountVariable(String amountInString) {
+        amount = Float.parseFloat(amountInString);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void setDateInPattern(EditText calendar) {
+        date = getLocalDateInPattern(calendar).toEpochDay();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public LocalDate getLocalDateInPattern(EditText calendar) {
+        return LocalDate.parse(calendar.getText(), getPattern());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public DateTimeFormatter getPattern() {
+        return DateTimeFormatter.ofPattern(DATE_FORMAT);
+    }
+
+    private void setProfit() {
+        isProfit = getSelectedProfitIconId() == getProfitIconId();
+    }
+
+    private int getProfitIconId() {
+        return R.id.profitTrue;
+    }
+
+    private int getSelectedProfitIconId() {
+        return getRadioGroup().getCheckedRadioButtonId();
+    }
+
+    private RadioGroup getRadioGroup() {
+        return root.findViewById(R.id.radioGroup);
     }
 
     public float getAmount() {
@@ -69,7 +150,7 @@ public class TransactionDataCollectorFromUser {
     }
 
     public boolean isProfit() {
-        return profit;
+        return isProfit;
     }
 
     public int getCategoryId() {
