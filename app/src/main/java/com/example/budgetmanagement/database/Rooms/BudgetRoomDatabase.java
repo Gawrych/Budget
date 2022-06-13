e369840709674fc2318545a69f9353214afe8c6c
Broslaws <pawelgawrych203@gmail.com>
2022-06-13 17:03:57 +0200

    Change database version to 46

--- app/src/main/java/com/example/budgetmanagement/database/Rooms/BudgetRoomDatabase.java
+++ app/src/main/java/com/example/budgetmanagement/database/Rooms/BudgetRoomDatabase.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Transaction.class, Category.class, Coming.class, History.class}, version = 44, exportSchema = false)
+@Database(entities = {Transaction.class, Category.class, Coming.class, History.class}, version = 46, exportSchema = false)
 public abstract class BudgetRoomDatabase extends RoomDatabase {
 
     public abstract TransactionDao transactionDao();
