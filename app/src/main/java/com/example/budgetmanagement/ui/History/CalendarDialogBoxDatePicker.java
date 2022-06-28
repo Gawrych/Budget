01904ec4637a821b382ea1ce0dc552f4abcf7f0b
Broslaws <pawelgawrych203@gmail.com>
2022-06-28 15:20:11 +0200

    Add getTodayDateFormat method and overload getDateInFormat

--- app/src/main/java/com/example/budgetmanagement/ui/History/CalendarDialogBoxDatePicker.java
+++ app/src/main/java/com/example/budgetmanagement/ui/History/CalendarDialogBoxDatePicker.java
@@ -1,15 +1,19 @@
 package com.example.budgetmanagement.ui.History;
 
-import static com.example.budgetmanagement.MainActivity.DATE_FORMAT;
+import static com.example.budgetmanagement.MainActivity.DEFAULT_DATE_FORMAT;
 
+import android.os.Build;
 import android.widget.EditText;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.budgetmanagement.R;
 import com.google.android.material.datepicker.MaterialDatePicker;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class CalendarDialogBoxDatePicker {
@@ -26,10 +30,23 @@ public class CalendarDialogBoxDatePicker {
     public void show(FragmentManager parentFragmentManager, EditText calendar) {
         if (!datePicker.isAdded()) {
             datePicker.show(parentFragmentManager, "DATE_PICKER");
-            datePicker.addOnPositiveButtonClickListener(selection -> {
-                SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
-                calendar.setText(sdf.format(selection));
-            });
+            datePicker.addOnPositiveButtonClickListener(selection -> calendar.setText(getDateInFormat(selection)));
         }
+    }
+
+    private String getDateInFormat(Long date) {
+        SimpleDateFormat sdf = new SimpleDateFormat(DEFAULT_DATE_FORMAT, Locale.getDefault());
+         return sdf.format(date);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String getDateInFormat(LocalDate date) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT);
+        return dtf.format(date);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public String getTodayDateInFormat() {
+        return getDateInFormat(LocalDate.now());
     }
 }
