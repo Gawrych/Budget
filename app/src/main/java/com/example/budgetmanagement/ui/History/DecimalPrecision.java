7f4645340474d97b7ca54b5713412bd32d672320
Broslaws <pawelgawrych203@gmail.com>
2022-06-28 13:41:47 +0200

    Create class to process data from amount EditText field

---
+++ app/src/main/java/com/example/budgetmanagement/ui/History/DecimalPrecision.java
@@ -0,0 +1,23 @@
+package com.example.budgetmanagement.ui.History;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class DecimalPrecision {
+
+    private String content;
+
+    public DecimalPrecision(String content) {
+        this.content = content;
+    }
+
+    public BigDecimal getParsedContent() {
+        return new BigDecimal(replaceAllCommaToDot(content)).setScale(2, RoundingMode.DOWN);
+    }
+
+    private String replaceAllCommaToDot(String numberToProcessing) {
+        String dot = ".";
+        String comma = ",";
+        return numberToProcessing.replaceAll(comma, dot);
+    }
+}
