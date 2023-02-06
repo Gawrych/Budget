14eed8129c2aacadd0f48e31cbee04e2b60051f4
broslaw <pawelgawrych203@gmail.com>
2023-02-07 00:44:35 +0100

    Create system to get value from field or set error if field is empty

--- app/src/main/java/com/example/budgetmanagement/ui/coming/AddNewComingElement.java
+++ app/src/main/java/com/example/budgetmanagement/ui/coming/AddNewComingElement.java
@@ -3,26 +3,36 @@ package com.example.budgetmanagement.ui.coming;
 import android.app.DatePickerDialog;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.budgetmanagement.R;
+import com.example.budgetmanagement.database.rooms.Transaction;
+import com.example.budgetmanagement.database.viewmodels.ComingViewModel;
+import com.example.budgetmanagement.database.viewmodels.TransactionViewModel;
 import com.example.budgetmanagement.databinding.AddNewComingFragmentBinding;
 import com.example.budgetmanagement.ui.utils.AppIconPack;
 import com.example.budgetmanagement.ui.utils.CategoryBottomSheetSelector;
 import com.example.budgetmanagement.ui.utils.DateProcessor;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.maltaisn.icondialog.data.Icon;
 import com.maltaisn.icondialog.pack.IconPack;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class AddNewComingElement extends Fragment {
@@ -35,6 +45,14 @@ public class AddNewComingElement extends Fragment {
     private CategoryBottomSheetSelector categoryPicker;
     private int categoryId;
     private IconPack iconPack;
+
+    private boolean successCollectedData = true;
+    private String title;
+    private String amount;
+    private String category;
+    private String startDate;
+    private String period;
+    private String endDate;
 
     public static AddNewComingElement newInstance(int comingId) {
         Bundle bundle = new Bundle();
@@ -59,13 +77,28 @@ public class AddNewComingElement extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         binding.cyclicalSwitch.setOnCheckedChangeListener((button, isChecked) ->
                 disableOrEnabledCyclicalFields(isChecked));
         categoryPicker = new CategoryBottomSheetSelector(this);
         iconPack = ((AppIconPack) requireActivity().getApplication()).getIconPack();
-
         prepareFields();
+
+        binding.acceptButton.setOnClickListener(v -> {
+            this.title = collect(binding.titleLayout);
+            this.amount = collect(binding.amountLayout);
+            this.category = collect(binding.categorySelectorLayout);
+            this.startDate = collect(binding.startDateLayout);
+
+            if (binding.cyclicalSwitch.isChecked()) {
+                this.period = collect(binding.periodPickerLayout);
+                this.endDate = collect(binding.endDateLayout);
+            }
+
+            if (successCollectedData) {
+
+            }
+            successCollectedData = true;
+        });
     }
 
     private void disableOrEnabledCyclicalFields(boolean enabled) {
@@ -130,4 +163,76 @@ public class AddNewComingElement extends Fragment {
         DatePickerDialog datePickerDialog = setDatePickerDialog(field, selectedField);
         field.setOnClickListener(v -> datePickerDialog.show());
     }
+// to New class
+    private String collect(TextInputLayout inputLayout) {
+        EditText editText = inputLayout.getEditText();
+        String value = editText == null ? "" : editText.getText().toString();
+
+        if (value.isEmpty()) {
+            setErrorMessage(inputLayout);
+            successCollectedData = false;
+        }
+
+        return value;
+    }
+
+
+    private void setErrorMessage(TextInputLayout inputLayout) {
+        inputLayout.setError(getString(R.string.empty_field));
+    }
+
+//    public ArrayList<Long> getNextDates() {
+//        ArrayList<Long> allDatesToCreateNewComing = new ArrayList<>();
+//        SwitchMaterial cyclicalSwitch = fieldsInterface.getCyclicalSwitch();
+//
+//        if (!cyclicalSwitch.isChecked()) {
+//            allDatesToCreateNewComing.add(getStartDate());
+//            return allDatesToCreateNewComing;
+//        }
+//
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTimeInMillis(getStartDate());
+//        long nextDate = calendar.getTimeInMillis();
+//
+//        Integer valueFromTimeBetweenMap = timeBetweenValues.get(timeBetweenExecutePicker.getText().toString());
+//        if (valueFromTimeBetweenMap == null) {
+//            return allDatesToCreateNewComing;
+//        }
+//
+//        int timeBetween = valueFromTimeBetweenMap;
+//        long endDate = getEndDate();
+//
+//        while (nextDate <= endDate) {
+//            allDatesToCreateNewComing.add(nextDate);
+//            calendar.add(timeBetween, 1);
+//            nextDate = calendar.getTimeInMillis();
+//        }
+//
+//        return allDatesToCreateNewComing;
+//    }
+//
+//    private void submitCyclical(int amountOfNewDates) {
+//        int MIN_AMOUNT_OF_DATES_TO_CREATE_CYCLICAL_COMING = 2;
+//        if (amountOfNewDates < MIN_AMOUNT_OF_DATES_TO_CREATE_CYCLICAL_COMING) {
+//            endDateLayout.setError(getString(R.string.not_enough_to_generate_cyclical_change_endDate_or_timeBetween));
+//            return;
+//        }
+//
+//        submitNewComingItemToDatabase(newComingDataCollector, dates);
+//
+//        String howMuchAdded = "Dodano " + amountOfNewDates + " transakcje";
+//        Toast.makeText(requireContext(), howMuchAdded, Toast.LENGTH_SHORT).show();
+//    }
+//
+//    private void submitNewComingItemToDatabase(NewComingDataCollector newComing, ArrayList<Long> dates) {
+//        TransactionViewModel transactionViewModel = new ViewModelProvider(this).get(TransactionViewModel.class);
+//        ComingViewModel comingViewModel = new ViewModelProvider(this).get(ComingViewModel.class);
+//
+//        long transactionId = transactionViewModel.insert(newComing.getTransaction());
+//
+//        for (Long date : dates) {
+//            comingViewModel.insert(newComing.getComing(transactionId, date));
+//        }
+//        close();
+//    }
 }
